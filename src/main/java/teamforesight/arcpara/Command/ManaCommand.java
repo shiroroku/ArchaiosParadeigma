9e9236b089f05517e400e0ffecac258350b8c0c5
shiroroku(Elise) <empresselise@proton.me>
2023-08-11 22:52:03 -0700

    updated mana commands

--- src/main/java/teamforesight/arcpara/Command/ManaCommand.java
+++ src/main/java/teamforesight/arcpara/Command/ManaCommand.java
@@ -9,11 +9,11 @@ import teamforesight.arcpara.Registry.CapabilityRegistry;
 
 public class ManaCommand {
 	public static void register (CommandDispatcher<CommandSourceStack> pDispatcher) {
-		pDispatcher.register(Commands.literal("arcpara").requires(cs -> cs.hasPermission(4)).then(Commands.literal("mana").then(Commands.argument("player", EntityArgument.player()).then(Commands.literal("set").then(Commands.argument("amount", FloatArgumentType.floatArg(0)).executes((cs -> {
+		pDispatcher.register(Commands.literal("arcpara").requires(cs -> cs.hasPermission(4)).then(Commands.argument("player", EntityArgument.player()).then(Commands.literal("mana").then(Commands.literal("set").then(Commands.argument("amount", FloatArgumentType.floatArg(0)).executes((cs -> {
 			CapabilityRegistry.getSpellCaster(EntityArgument.getPlayer(cs, "player")).ifPresent(cap -> cap.setMana(Math.min(FloatArgumentType.getFloat(cs, "amount"), cap.getMaxMana())));
 			CapabilityRegistry.sendCapabilityPacket(EntityArgument.getPlayer(cs, "player"));
 			return 0;
-		})))))).then(Commands.literal("max_mana").then(Commands.argument("player", EntityArgument.player()).then(Commands.literal("set").then(Commands.argument("amount", FloatArgumentType.floatArg(0)).executes((cs -> {
+		}))))).then(Commands.literal("max_mana").then(Commands.literal("set").then(Commands.argument("amount", FloatArgumentType.floatArg(0)).executes((cs -> {
 			CapabilityRegistry.getSpellCaster(EntityArgument.getPlayer(cs, "player")).ifPresent(cap -> cap.setMaxMana(FloatArgumentType.getFloat(cs, "amount")));
 			CapabilityRegistry.sendCapabilityPacket(EntityArgument.getPlayer(cs, "player"));
 			return 0;
